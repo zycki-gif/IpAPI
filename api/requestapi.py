3abd1fae21fc8a7db09d534f42389e2bb6931b32
zycki-gif <82342478+zycki-gif@users.noreply.github.com>
2022-02-21 12:17:47 -0300

    Update requestapi.py

--- api/requestapi.py
+++ api/requestapi.py
@@ -1,6 +1,10 @@
 import requests
 
 def get_location(query):
-    url = f"http://ip-api.com/json/{query}" 
-    response = requests.get(url).json()                              
+    """
+    Função para lidar com a requesição da API(http://ip-api.com/json/)
+    tomando como argumentos a str query e retorna os dados em JSON.
+    """
+    url = f"http://ip-api.com/json/{query}"  #3th party API
+    response = requests.get(url).json()    #data em formato JSON                           
     return response

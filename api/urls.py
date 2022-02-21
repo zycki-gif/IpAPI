396f2d9e3820627a50755011495f6775a92efeaf
zycki-gif <82342478+zycki-gif@users.noreply.github.com>
2022-02-21 12:22:29 -0300

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ from  api import views
 
 urlpatterns = [
     path(
-        route = 'api', #/api?query=
+        route = 'api', #/api?query=24.10.0.1
         view = views.location_detail,
     )
    

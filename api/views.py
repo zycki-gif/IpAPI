94c778603f88ba47a103522add92a21a1fe8da13
zycki-gif <82342478+zycki-gif@users.noreply.github.com>
2022-02-21 12:21:29 -0300

    Update views.py

--- api/views.py
+++ api/views.py
@@ -7,11 +7,15 @@ from . requestapi import get_location
 
 
 @api_view(['GET'])
-def location_detail(request):                                
-    serializer = Myserializer(data=request.GET)          
-    serializer.is_valid()                                  
-    query = serializer.validated_data['query']
-    ip_info = get_location(query) 
+def location_detail(request): 
+    """
+    Recebe a requesição e retorna os dados em JSON com 
+    o ip (query) fornecido.
+    """
+    serializer = Myserializer(data=request.GET)    # Passe os dados da solicitação para o Mtserializer()       
+    serializer.is_valid()                          # Retorna True se os campos forem validos              
+    query = serializer.validated_data['query']  
+    ip_info = get_location(query)   # Chama a função get_location que consulta a API
     return Response(ip_info)
 
 
